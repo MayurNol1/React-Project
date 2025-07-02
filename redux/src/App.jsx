a46b2fd498308b721cbc6858bc11c6c186bd608f
Mayur Nol <mayurnol111@gmail.com>
2025-07-02 12:36:09 +0530

    todo app with redux

---
+++ redux/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import ListTodo from './components/ListTodo'
+
+function App() {
+  
+
+  return (
+    <>
+     <AddTodo/>
+     <ListTodo/>
+    </>
+  )
+}
+
+export default App
