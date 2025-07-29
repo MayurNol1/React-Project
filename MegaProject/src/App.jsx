439cf7dc73290e2981d0331bf5713803ad055af2
Mayur Nol <mayurnol111@gmail.com>
2025-07-30 00:39:22 +0530

    new project

---
+++ MegaProject/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+      console.log(import.meta.env.VITE_APPWRITE_URL)
+      
+  return (
+    <>
+      <h1 className='bg-blue-900 text-4xl'>Hey its mayur</h1>
+    </>
+  )
+}
+
+export default App
